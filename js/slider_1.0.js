6ab370a9ce671565d1f290edaef9f878239a6161
mryl <mryl@163.com>
2019-07-10 08:44:52 +0800

    v1.01

--- js/slider_1.0.js
+++ js/slider_1.0.js
@@ -10,19 +10,26 @@
 */
 (function($) {
     $.fn.slider = function(options) {
-
+		var defaultOption={
+    		width:200,
+    		isShowMark:true,
+    		sliderDotSize:16,
+			barHeight:6,
+			step:20
+    	}
 		var opts = $.extend(true, {}, defaultOption, options);
 
         // if (!this.length) {
         //     return this;
         // }
         function setValue(left,min,max,obj){
-        	var value = left/(opts.width-opts.sliderDotSize)*(max-min);
+        	var value = left/(opts.width-opts.sliderDotSize)*(max-min)+min;
         	obj.value = parseInt(value);
         }
 
     	function creatDom(min,max,step,obj){
 			if(max-min<step){
+				
 				return "<span>参数错误</span>";
 			}else{
 
@@ -61,16 +68,15 @@
 				sliderWrap.append(sliderBar);
 				sliderBar.append(sliderDot);
 
+				if((max-min)%step!==0) opts.isShowMark = false;
 				if(opts.isShowMark){
 					var str = "<div class=\"sliderMark\">";
-						step-=1;
-
-					for(i=0;i<=step;i++){
+					var sectionLength = (max-min)/step;
+					for(i=0;i<=sectionLength;i++){
 						var barWidth = opts.width-opts.sliderDotSize;
-						var leftValue = barWidth/step;
-						leftValue=leftValue*i+opts.sliderDotSize/2;
+						var leftValue = barWidth/sectionLength*i+opts.sliderDotSize/2;
 
-						splitValue = (opts.width/step)*i*(max-min)/opts.width;
+						splitValue = min+step*i;
 
 						str+="<dl style='left:"+leftValue+"px'>"+
 									"<dt></dt>"+
@@ -87,25 +93,19 @@
 
     	}
 
-    	var defaultOption={
-    		width:200,
-    		isShowMark:true,
-    		sliderDotSize:16,
-				barHeight:6,
-				step:2
-    	}
+    	
         
         this.each(function() {
             var $this = $(this);
             var min = parseInt(this.min)||0;
             var max = parseInt(this.max)||100;
-            var num = parseInt(opts.step)||5;
-            var sliderDom = creatDom(min,max,num,this);
+            var step = parseInt(opts.step);
+            var sliderDom = creatDom(min,max,step,this);
             $(sliderDom).insertAfter($this);
 
 			$this.change(function(){
 				var value = this.value;
-				var left = value/(max-min)*(opts.width-16);
+				var left = (value-min)/(max-min)*(opts.width-opts.sliderDotSize);
 				sliderDom.find(".sliderDot").css("left",left);
 			})
 
